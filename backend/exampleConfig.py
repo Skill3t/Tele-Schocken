364121862d46a72dc5b83a5ae761622e0579058b
Skill3t <larslengersdorf@gmail.com>
2020-03-22 21:55:23 +0100

    pep8 adjustment

--- backend/exampleConfig.py
+++ backend/exampleConfig.py
@@ -5,8 +5,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 
 
-
-
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'geheim'
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://test:test@127.0.0.1:8889/testdb'
