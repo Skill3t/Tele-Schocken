92d7f0630077f4065ca599639b72dc62f929f2af
Skill3t <larslengersdorf@gmail.com>
2020-03-23 13:27:53 +0100

    endpoint for start

--- backend/app/api/endpoint.py
+++ backend/app/api/endpoint.py
@@ -68,6 +68,21 @@ def set_game_user(gid):
     return jsonify(game.to_dict())
 
 
+# Start the Game
+@bp.route('/game/<gid>/start', methods=['POST'])
+def start_game(gid):
+    game = Game.query.filter_by(UUID=gid).first()
+    if game is None:
+        response = jsonify()
+        response.status_code = 404
+        return response
+    game.status = Status.STARTED
+    db.session.add(game)
+    db.session.commit()
+    response = jsonify()
+    response.status_code = 201
+    return response
+
 # roll dice
 @bp.route('/game/<gid>/user/<uid>/dice', methods=['POST'])
 def roll_dice(gid, uid):
@@ -77,7 +92,10 @@ def roll_dice(gid, uid):
         response = jsonify()
         response.status_code = 404
         return response
-
+    if user.game_id != game.id:
+        response = jsonify()
+        response.status_code = 404
+        return response
     data = request.get_json() or {}
     if 'dice1' in data:
         if data['dice1']:
@@ -111,7 +129,10 @@ def turn_dice(gid, uid):
         response = jsonify()
         response.status_code = 404
         return response
-
+    if user.game_id != game.id:
+        response = jsonify()
+        response.status_code = 404
+        return response
     data = request.get_json() or {}
     if 'count' in data:
         if data['count'] == 1:
@@ -154,28 +175,39 @@ def turn_dice(gid, uid):
 
 # transfer chips
 @bp.route('/game/<gid>/user/chips', methods=['POST'])
-def transfer_chips(gid, uid):
+def transfer_chips(gid):
     game = Game.query.filter_by(UUID=gid).first()
-    user = User.query.get_or_404(uid)
     if game is None:
         response = jsonify()
         response.status_code = 404
         return response
     data = request.get_json() or {}
-    if 'dice1' in data:
-        if data['dice1']:
-            user.dice1 = randint(1, 6)
-    if 'dice2' in data:
-        if data['dice2']:
-            user.dice2 = randint(1, 6)
-    if 'dice3' in data:
-        if data['dice3']:
-            user.dice3 = randint(1, 6)
-    fallen = decision(game.changs_of_fallling_dice)
-    response = jsonify(fallen=fallen, dice1=user.dice1, dice2=user.dice2, dice3=user.dice3)
+    # requierd attribute not included
+    if 'count' not in data or 'target' not in data:
+        response = jsonify()
+        response.status_code = 400
+        return response
+    # transfer from user a to B
+    if 'count' in data and 'source' in data and 'target' in data:
+        userA = User.query.get_or_404(data['source'])
+        userB = User.query.get_or_404(data['target'])
+        if userA.chips >= data['count']:
+            userA.chips = userA.chips - data['count']
+            userB.chips = userB.chips + data['count']
+            db.session.add(userA)
+            db.session.add(userB)
+            db.session.commit()
+    # transfer from stack to user B
+    if 'count' in data and 'stack' in data and 'target' in data:
+        userB = User.query.get_or_404(data['target'])
+        if game.stack >= data['count']:
+            game.stack = game.stack - data['count']
+            userB.chips = userB.chips + data['count']
+            db.session.add(game)
+            db.session.add(userB)
+            db.session.commit()
+    response = jsonify()
     response.status_code = 201
-    db.session.add(user)
-    db.session.commit()
     return response
 
 
