de27c9e3f4ade6f751c56d3af6717030533052d9
Skill3t <larslengersdorf@gmail.com>
2020-07-05 19:26:57 +0200

    undo schedular change

--- backend/app/api/scheduler.py
+++ backend/app/api/scheduler.py
@@ -33,7 +33,7 @@ def schedulerdeletegame():
 
 scheduler = BackgroundScheduler()
 # 3.600 seconds are 1 houer
-scheduler.add_job(func=schedulerdeletegame, trigger="interval", seconds=5)
+scheduler.add_job(func=schedulerdeletegame, trigger="interval", seconds=3.600)
 
 scheduler.start()
 
