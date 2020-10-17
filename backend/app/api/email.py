0aeb9d74f705ecf3b53091c58aee870c314a513e
Skill3t <larslengersdorf@gmail.com>
2020-10-17 19:28:47 +0200

    add Semaphore for email and deleting old games to stop duplicates

--- backend/app/api/email.py
+++ backend/app/api/email.py
@@ -2,20 +2,25 @@ from app import app
 from app import mail
 from flask_mail import Message
 from flask import render_template
+from threading import Semaphore
 
 
 def sendMail(body):
-    with app.app_context():
-        msg = Message('New Teleschocken Game', sender='lars@tele-schocken.de', recipients=['lars@tele-schocken.de'])
-        msg.body = body
-        mail.send(msg)
-    return
+    lock = Semaphore()
+    with lock:
+        with app.app_context():
+            msg = Message('New Teleschocken Game', sender='lars@tele-schocken.de', recipients=['lars@tele-schocken.de'])
+            msg.body = body
+            mail.send(msg)
+        return
 
 
 def sendFeedbackMail(browser, body, email):
-    with app.app_context():
-        msg = Message('Feedback Teleschocken', sender='lars@tele-schocken.de', recipients=['lars@tele-schocken.de'])
-        msg.body = '{} /n {} {}'.format(browser, body, email)
-        msg.html = render_template('mail.html', message=body, email=email, browser=browser)
-        mail.send(msg)
-    return
+    lock = Semaphore()
+    with lock:
+        with app.app_context():
+            msg = Message('Feedback Teleschocken', sender='lars@tele-schocken.de', recipients=['lars@tele-schocken.de'])
+            msg.body = '{} /n {} {}'.format(browser, body, email)
+            msg.html = render_template('mail.html', message=body, email=email, browser=browser)
+            mail.send(msg)
+        return
--- backend/app/api/scheduler.py
+++ backend/app/api/scheduler.py
@@ -1,5 +1,6 @@
 import atexit
 from app import app, db
+from threading import Semaphore
 
 from apscheduler.schedulers.background import BackgroundScheduler
 from app.models import Game, Statistic
@@ -10,26 +11,28 @@ from dateutil.relativedelta import relativedelta
 def schedulerdeletegame():
     todayDate = datetime.now()
     delta = relativedelta(days=-1)
-    one_hour = todayDate + delta
-    old_games = Game.query.filter(Game.refreshed <= one_hour).all() # noqa
-    for old_game in old_games:
-        # print('old_game.refreshed:{}'.format(old_game.refreshed))
-        # print('delte: {}'.format(old_game))
-        stat = Statistic()
-        stat.usercount = len(old_game.users)
-        stat.started = old_game.started
-        stat.refreshed = old_game.refreshed
-        stat.halfcount = old_game.halfcount
-        stat.schockoutcount = old_game.schockoutcount
-        stat.fallling_dice_count = old_game.fallling_dice_count
-        stat.changs_of_fallling_dice = old_game.changs_of_fallling_dice
-        stat.throw_dice_count = old_game.throw_dice_count
-        db.session.add(stat)
-        for user in old_game.users:
-            db.session.delete(user)
-        db.session.delete(old_game)
-        db.session.commit()
-    db.session.commit()
+    one_day = todayDate + delta
+    old_games = Game.query.filter(Game.refreshed <= one_day).all() # noqa
+    db.session.begin()
+    lock = Semaphore()
+    with lock:
+        # critical section
+        for old_game in old_games:
+            # print('old_game.refreshed:{}'.format(old_game.refreshed))
+            stat = Statistic()
+            stat.usercount = len(old_game.users)
+            stat.started = old_game.started
+            stat.refreshed = old_game.refreshed
+            stat.halfcount = old_game.halfcount
+            stat.schockoutcount = old_game.schockoutcount
+            stat.fallling_dice_count = old_game.fallling_dice_count
+            stat.changs_of_fallling_dice = old_game.changs_of_fallling_dice
+            stat.throw_dice_count = old_game.throw_dice_count
+            db.session.add(stat)
+            for user in old_game.users:
+                db.session.delete(user)
+            db.session.delete(old_game)
+            db.session.commit()
 
 
 scheduler = BackgroundScheduler()
