5cef369152377f5a3b6f0005aebe468a62a6dfb2
Skill3t <larslengersdorf@gmail.com>
2020-11-10 08:14:12 +0100

    add donate possibilities

---
+++ backend/app/static/scripts/donate.js
@@ -0,0 +1,41 @@
+function donate(){
+  var acc = document.getElementsByClassName("accordion");
+  var i;
+
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      }
+    });
+  }
+  // Get the modal
+  var modal = document.getElementById("donateModal");
+
+  // Get the button that opens the modal
+  var btn = document.getElementById("donateBtn");
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementById("donateclose");
+
+  // When the user clicks the button, open the modal
+  btn.onclick = function() {
+    modal.style.display = "block";
+  }
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+}
