e953596bc6241687b05091270416e26c0a75d50e
Wiiilmaa <github@jenswilmer.de>
2023-11-12 21:53:08 +0100

    Add import for os.environ

--- backend/app/api/game_endpoints.py
+++ backend/app/api/game_endpoints.py
@@ -1,6 +1,7 @@
 from app.api import bp
 from app import app, db
 
+from os import environ
 from flask_socketio import emit, join_room
 from flask import jsonify
 from flask import request
@@ -19,7 +20,7 @@ from flask_socketio import SocketIO
 # different async modes, or leave it set to None for the application to choose
 # the best option based on installed packages.
 async_mode = "gevent"
-socketio = SocketIO(app, async_mode=async_mode, cors_allowed_origins="https://"+os.environ.get('SERVER_NAME'))
+socketio = SocketIO(app, async_mode=async_mode, cors_allowed_origins="https://"+environ.get('SERVER_NAME'))
 
 @socketio.on('connect', namespace='/game')
 def test_connect():
