a46b3a65241a68b2a56ce0067c17d9eaa35ec15e
Wiiilmaa <github@jenswilmer.de>
2023-11-12 22:04:35 +0100

    Experimente

--- backend/app/api/game_endpoints.py
+++ backend/app/api/game_endpoints.py
@@ -20,8 +20,9 @@ from flask_socketio import SocketIO
 # different async modes, or leave it set to None for the application to choose
 # the best option based on installed packages.
 async_mode = "gevent"
-server_name = environ.get('SERVER_NAME')
-socketio = SocketIO(app, async_mode=async_mode, cors_allowed_origins="https://" + server_name)
+server_name = "https://" + environ.get('SERVER_NAME')
+print('Servername ' + server_name))
+socketio = SocketIO(app, async_mode=async_mode, cors_allowed_origins=server_name)
 
 @socketio.on('connect', namespace='/game')
 def test_connect():
